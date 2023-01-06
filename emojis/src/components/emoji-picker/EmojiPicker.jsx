a0c1a9729213c750f3159db91d11a653214796c8
jeici21 <david92_jc@yahoo.es>
2023-01-06 15:34:58 -0500

    Creada función EmojiButton.

--- emojis/src/components/emoji-picker/EmojiPicker.jsx
+++ emojis/src/components/emoji-picker/EmojiPicker.jsx
@@ -1,29 +1,60 @@
 import { forwardRef, useState } from "react";
 import { data as emojiList } from "./Data";//importando los emojis del objeto
+import EmojiButton from "./EmojiButton";
+import EmojiSearch from "./EmojiSearch";
 
 export function EmojiPicker(props, inputRef) {
     const [isOpen, setIsOpen] = useState(false);
-    const [emojis, setEmojis] = useState(emojiList);
+    const [emojis, setEmojis] = useState([...emojiList]);
     const handleClickOpen = () => {
         setIsOpen(!isOpen);//abriendo y cerrando la lista de emojis
     }
-    const EmojiPickerContainer = () => {
-        return (
-            <div>
-                <input />
+    const handleSearch = (e) => {
+        const q = e.target.value;
+        if (!!q) {//si q no está vacío
+            const search = emojiList.filter((emoji) => {
+                return (
+                    emoji.name.toLowerCase().includes(q) ||
+                    emoji.keywords.toLowerCase().includes(q)
+                );
+            });//si la búsqueda coincide con el nombre o keyword de un emoji
+            setEmojis(search);
+        } else {
+            setEmojis(emojiList);
+        }
+    }
+    /*     const EmojiPickerContainer = () => {
+    
+            return (
                 <div>
-                    {emojiList.map((emoji) => (
-                        <div>{emoji.symbol}</div>
-                    ))}
+                    <EmojiSearch onSearch={handleSearch} />
+                    <div>
+                        {emojis.map((emoji) => (
+                            <div key={emoji.symbol}>{emoji.symbol}</div>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        } */
+    const handleOnClickEmoji = () => {
+        
     }
 
     return (
         <div>
             <button onClick={handleClickOpen}>😒</button>
-            {isOpen ? <EmojiPickerContainer /> : ""}
+            {isOpen ? (
+                <div>
+                    <EmojiSearch onSearch={handleSearch} />
+                    <div>
+                        {emojis.map((emoji) => (
+                            <EmojiButton key={emoji.symbol} emoji={emoji} onClick={handleOnClickEmoji} />
+                        ))}
+                    </div>
+                </div>
+            ) : (
+                ""
+            )}
         </div>
     );
 }
---
+++ emojis/src/components/emoji-picker/EmojiSearch.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export default function EmojiSearch({ onSearch }) {
+    const [value, setValue] = useState("");
+    const handleChange = (e) => {
+        setValue(e.target.value);
+        onSearch(e);
+    }
+    return <input type="text" onChange={handleChange} value={value} />;
+}
