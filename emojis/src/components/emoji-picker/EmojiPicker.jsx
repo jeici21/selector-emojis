1547e3310721028ac1891384ebe4446d58c95c76
jeici21 <david92_jc@yahoo.es>
2023-01-05 23:45:30 -0500

    Creando lista desplegable de emojis.

--- emojis/src/components/emoji-picker/EmojiPicker.jsx
+++ emojis/src/components/emoji-picker/EmojiPicker.jsx
@@ -1,3 +1,31 @@
-export default function EmojiPicker() {
-    return <div>Hola</div>;
+import { forwardRef, useState } from "react";
+import { data as emojiList } from "./Data";//importando los emojis del objeto
+
+export function EmojiPicker(props, inputRef) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [emojis, setEmojis] = useState(emojiList);
+    const handleClickOpen = () => {
+        setIsOpen(!isOpen);//abriendo y cerrando la lista de emojis
+    }
+    const EmojiPickerContainer = () => {
+        return (
+            <div>
+                <input />
+                <div>
+                    {emojiList.map((emoji) => (
+                        <div>{emoji.symbol}</div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <button onClick={handleClickOpen}>😒</button>
+            {isOpen ? <EmojiPickerContainer /> : ""}
+        </div>
+    );
 }
+
+export default forwardRef(EmojiPicker);//exportando referencia
