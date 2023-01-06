1b67283e812d9fac8c39250fb1495dd00b5be1c6
jeici21 <david92_jc@yahoo.es>
2023-01-05 23:02:16 -0500

    Implementando useRef.

--- emojis/src/components/emoji-picker/EmojiPickerInput.jsx
+++ emojis/src/components/emoji-picker/EmojiPickerInput.jsx
@@ -1,8 +1,11 @@
+import { useRef } from "react";
 import EmojiPicker from "./EmojiPicker";
 
 export default function EmojiPickerInput() {
+    const refInput = useRef(null);//obteniendo referencia del input para usarlo como objeto javascript
+
     return <div>
-        <input />
-        <EmojiPicker />
+        <input ref={refInput} />
+        <EmojiPicker ref={refInput} />
     </div>
 }
